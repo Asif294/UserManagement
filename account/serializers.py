3932771f445b4a656abe401dcb11d243da996cbf
Asif294 <asifurrahman677294@gmail.com>
2025-10-12 11:59:55 +0600

    email

--- account/serializers.py
+++ account/serializers.py
@@ -1,23 +1,28 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-class  RegistrationSeriarizer(serializers.ModelSerializer):
-    confirm_password=serializers.CharField(required=True)
+
+class RegistrationSeriarizer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(required=True)
+
     class Meta:
-        model=User
-        fields=['username','first_name','last_name','email','password','confirm_password']
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'confirm_password']
 
     def save(self):
-        username=self.validated_data('username')
-        email=self.validated_data('email')
-        password=self.validated_data('password')
-        confirm_password=self.validated_data('confirm_password')
-        
-        if password !=confirm_password:
-            raise serializers.ValidationError({'error' : "password doesn't mactched"})
+        username = self.validated_data['username']
+        first_name=self.validated_data['first_name']
+        last_name=self.validated_data['last_name']
+        email = self.validated_data['email']
+        password = self.validated_data['password']
+        confirm_password = self.validated_data['confirm_password']
+
+        if password != confirm_password:
+            raise serializers.ValidationError({'error': "Passwords don't match"})
         if User.objects.filter(email=email).exists():
-            raise serializers.ValidationError({'error' : "Email Already exists "})
-        
-        account=User(username=username,email=email)
+            raise serializers.ValidationError({'error': "Email already exists"})
+
+        account = User(username=username,first_name=first_name,last_name=last_name, email=email  )
         account.set_password(password)
         account.save()
         return account
+ 
--- account/views.py
+++ account/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
-from .import serializers
+from . import serializers
+from rest_framework.response import Response
+
 class UserRegistrationApiView(APIView):
-    serializer_class =serializers.RegistrationSeriarizer
-    
+    serializer_class = serializers.RegistrationSeriarizer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Registration successful!'})
+        return Response(serializer.errors, status=400)
